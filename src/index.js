11b70e5188137673d0024c368e980883e3f7fcf2
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-01 14:35:57 -0800

    Create basic express server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 7000;
+const url = process.env.URL || 'localhost';
+
+app.get('/:videoId', (req, res) => {
+  res.send(`You requested video ${req.params.videoId}`);
+});
+
+app.listen(port, () => console.log(`Server on ${url} using port ${port}`));
