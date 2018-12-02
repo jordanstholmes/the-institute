d06f3e497331ca651e8be52fd43777f1c7cc7aff
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-01 22:35:11 -0800

    Refactor database index.js file exports

--- database/index.js
+++ database/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const getVideoById = require('./controllers/video.js');
+const getQuizByName = require('./controllers/quiz.js');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost/institute';
 mongoose.connect(dbUrl, { useNewUrlParser: true });
@@ -8,4 +10,4 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongoose connection error:'));
 db.once('open', () => console.log('Connected to mongo server on', dbUrl));
 
-module.exports = db;
+module.exports = { getVideoById, getQuizByName };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const db = require('../database/index.js');
-const getVideoById = require('../database/controllers/video.js');
-const getQuizByName = require('../database/controllers/quiz.js');
 
 const port = process.env.PORT || 7000;
 const url = process.env.URL || 'localhost';
@@ -13,7 +11,7 @@ const logging = process.env.NODE_ENV === 'production' ? morgan('short') : morgan
 // temporarily hardcode videoId: '5c032bc929a362516c22b2fe'
 app.use(logging);
 app.get('/videos/:videoId', (req, res) => {
-  getVideoById('5c032bc929a362516c22b2fe')
+  db.getVideoById('5c032bc929a362516c22b2fe')
     .then(video => res.send(video))
     .catch((err) => {
       console.err('getVideoById error:', err);
@@ -23,7 +21,7 @@ app.get('/videos/:videoId', (req, res) => {
 
 // temporarily hardcode quizName: 'SESSION 9 QUIZ'
 app.get('/quizzes/:quizName', (req, res) => {
-  getQuizByName('SESSION 9 QUIZ')
+  db.getQuizByName('SESSION 9 QUIZ')
     .then(quiz => res.send(quiz))
     .catch((err) => {
       console.err('getQuizByName error:', err);
