3b9ee619ef9ded0de53b97d1a83f664fad806b4e
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-02 19:28:19 -0700

    Rename configuration dir to test-config

--- test-config/jest.config.js
+++ test-config/jest.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/configuration/setupEnzyme.js'],
+  setupFilesAfterEnv: ['<rootDir>/test-config/setupEnzyme.js'],
   rootDir: path.join(__dirname, '../'),
 };
