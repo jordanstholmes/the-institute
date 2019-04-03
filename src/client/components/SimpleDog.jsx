ee182430d7fddb9fdba898fdb7757e969099bdac
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-03 11:47:40 -0700

    Add key to react array to avoid waring msg

--- src/client/components/SimpleDog.jsx
+++ src/client/components/SimpleDog.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const dogs = ['pal', 'buddy', 'baby'];
 
 const SimpleDog = () => {
-  const dogDivs = dogs.map(dog => <div className="dog">{dog}</div>);
+  const dogDivs = dogs.map(dog => <div key={dog} className="dog">{dog}</div>);
   return (
     <div className="dogWrapper">
       {dogDivs}
