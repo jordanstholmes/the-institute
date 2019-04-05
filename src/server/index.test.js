59e87c795d2d269098de875283ad60f62635a864
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-05 10:26:34 -0700

    Factor express out of server index file for testing purposes

--- src/server/index.test.js
+++ src/server/index.test.js
@@ -1,16 +1,27 @@
 import request from 'supertest';
-import express from 'express';
+// import express from 'express';
+import app from './index';
 
-const app = express();
+// const app = express();
 
-app.get('/user', (req, res) => {
-  res.status(200).json({ name: 'john' });
-});
+// app.get('/user', (req, res) => {
+//   res.status(200).json({ name: 'john' });
+// });
 
-describe('GET /user', () => {
+// describe('GET /user', () => {
+//   it('responds with json', (done) => {
+//     request(app)
+//       .get('/user')
+//       .set('Accept', 'application/json')
+//       .expect('Content-Type', /json/)
+//       .expect(200, done);
+//   });
+// });
+
+describe( 'GET /videos/:videoId', () => {
   it('responds with json', (done) => {
     request(app)
-      .get('/user')
+      .get('/vidoes/5c06c63a585ed5090417a6de')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
