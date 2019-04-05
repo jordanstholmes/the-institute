cad72fda0bc685ea93002d12ac9f516c99190bbc
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-05 09:24:09 -0700

    Render 3 mock VideoList images

--- src/client/components/VideoList.jsx
+++ src/client/components/VideoList.jsx
@@ -4,6 +4,24 @@ import styled from 'styled-components';
 
 import VideoListItem from './VideoListItem';
 
+const mock = [
+  {
+    title: '9.5 Getting Past the Gatekeeper',
+    source: 'https://player.vimeo.com/video/109855756?color=ff0179&title=0&byline=0&portrait=0',
+    _id: '123',
+  },
+  {
+    title: '4.1 The Got-A-Minute Manager',
+    source: 'https://player.vimeo.com/video/109259708?title=0&byline=0&portrait=0',
+    _id: '456',
+  },
+  {
+    title: '5.3 I am a fake title',
+    source: 'https://player.vimeo.com/video/109259708?title=0&byline=0&portrait=0',
+    _id: '456',
+  },
+];
+
 const Div = styled.div`
   height: 506px;
   width: 100%;
@@ -11,9 +29,9 @@ const Div = styled.div`
 `;
 
 const VideoList = ({ videos }) => (
-  <div>
-    {videos.map(video => <VideoListItem key={video._id} video={video} />)}
-  </div>
+  <Div>
+    {mock.map(video => <VideoListItem key={video._id} video={video} />)}
+  </Div>
 );
 
 export default VideoList;
