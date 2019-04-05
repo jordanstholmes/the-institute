3a89435dca788f4180afebfe21bdfd41eabe4b18
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-04 17:28:55 -0700

    Add bare skeleton for VideoList and VideoListItem

--- src/client/components/VideoList.jsx
+++ src/client/components/VideoList.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import VideoListItem from './VideoListItem';
+
 const Div = styled.div`
   height: 506px;
   width: 100%;
   background-color: #e7e6e6;
 `;
 
-const VideoList = () => {
-  return  <Div />
-};
+const VideoList = ({ videos }) => (
+  <div>
+    {videos.map(video => <VideoListItem key={video._id} video={video} />)}
+  </div>
+);
 
 export default VideoList;
