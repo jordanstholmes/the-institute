71972f5aa57e66458de4b2c1f8e14ff3eeb693c8
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-19 09:31:43 -0700

    Refactor seeding script to run in 5 min with correct commas and brackets

--- database/seedData/index.js
+++ database/seedData/index.js
@@ -1,13 +1,42 @@
-const generateVideo = require('./generateSeedData.js');
+const faker = require('faker');
 
-/*
-Write function to write seed file
-      > Do it with write file and small amount
-      > run with npm script
-      > Switch to console and Use pipe and gzip
-      > unzip it and check it looks right
-> Can this work with a 1k, then 1mil, then 5mil
-*/
+const VIDEO_URLS = [
+  'https://player.vimeo.com/video/109855756?color=ff0179&title=0&byline=0&portrait=0',
+  'https://player.vimeo.com/video/109259708?title=0&byline=0&portrait=0',
+  'https://vimeo.com/238981026/b5de19aeb2',
+];
+
+const MAX_NUM_OF_MODULES = 12; // arbitrarily chosen
+const MAX_SUBMODULES = 15; // arbitrarily chosen
+const BATCH_SIZE = 1000;
+
+const generateVideos = (startId, batchSize) => {
+  const videos = [];
+  let randomIdx;
+  let randomVideoUrl;
+  let randomTitle;
+  let randomModuleNum;
+  let randomSubModuleNum;
+
+  for (let i = 0; i < batchSize; i += 1) {
+    randomIdx = Math.floor(Math.random() * VIDEO_URLS.length);
+    randomVideoUrl = VIDEO_URLS[randomIdx];
+    randomTitle = faker.company.catchPhrase();
+
+    // +1 below is so so that the minimum module value is 1
+    randomModuleNum = Math.floor(Math.random() * MAX_NUM_OF_MODULES) + 1;
+    randomSubModuleNum = Math.floor(Math.random() * MAX_SUBMODULES);
+
+    videos.push(JSON.stringify({
+      id: startId + i,
+      source: randomVideoUrl,
+      title: randomTitle,
+      moduleNum: randomModuleNum,
+      submoduleNum: randomSubModuleNum,
+    }));
+  }
+  return videos.join(',\n');
+};
 
 const writeSeedFile = (callback) => {
   // get value of first arg
@@ -26,27 +55,27 @@ const writeSeedFile = (callback) => {
   // Write to file through stdout
   const LOG_FREQUENCY = 100000;
   console.log('[');
+  console.log(generateVideos(1, 1));
 
   let i = 0;
   const { stdout } = process;
 
   const writeToStdOut = () => {
     let hasSpace = true;
+    let videos;
 
-    while (i <= numOfRecords && hasSpace) {
-      i += 1;
-      let video = generateVideo();
-      video.id = i;
-      video = JSON.stringify(video);
+    while (i < numOfRecords && hasSpace) {
+      i += BATCH_SIZE;
+      videos = generateVideos(i, BATCH_SIZE);
 
       if (i % LOG_FREQUENCY === 0) {
-        console.error('Wrote:\n', video);
+        console.error('Wrote:\n', i);
       }
 
       if (i === numOfRecords) {
-        stdout.write(`${video}\n`, 'utf8', callback);
+        stdout.write(`,${videos}`, 'utf8', callback);
       } else {
-        hasSpace = stdout.write(`${video}\n,`, 'utf8');
+        hasSpace = stdout.write(`,${videos}`, 'utf8');
       }
     }
     if (i < numOfRecords) stdout.once('drain', writeToStdOut);
@@ -56,8 +85,13 @@ const writeSeedFile = (callback) => {
 };
 
 writeSeedFile(() => {
-  console.log(']');
+  console.log('\n]');
 
   const runTime = process.uptime();
   console.error(`seed generation took ${runTime / 60} minutes`);
 });
+
+// first needs no preceding comma
+// every other group needs a preceeding comma
+// OR
+// every group needs a comma after except the final
