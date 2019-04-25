1eee8c6c5fa768e5dee4f3b59f8067f94e1a8ee3
Jordan Holmes <jordanstholmes@gmail.com>
2019-04-24 22:59:29 -0700

    Add basic Artillery stress test

---
+++ test-config/generate-video-ids.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const ONE_MILLION = 1e6;
+const NINE_MILLION = 9e6;
+const writer = fs.createWriteStream(path.join(__dirname, 'video_ids.csv'));
+
+let videoId;
+
+for (let i = 0; i < ONE_MILLION; i += 1) {
+  videoId = Math.floor(Math.random() * ONE_MILLION) + NINE_MILLION; // number from 9 - 10 million
+  writer.write(`${videoId}\n`);
+}
