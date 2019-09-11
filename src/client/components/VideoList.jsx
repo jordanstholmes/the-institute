74ddb2aef746171a5bb08c2f872f991829f7f85b
Jordan Holmes <jordanstholmes@gmail.com>
2019-09-10 22:41:30 -0700

    Fix basic VideoList test

--- src/client/components/VideoList.jsx
+++ src/client/components/VideoList.jsx
@@ -4,24 +4,6 @@ import styled from 'styled-components';
 
 import VideoListItem from './VideoListItem';
 
-const mock = [
-  {
-    title: '9.5 Getting Past the Gatekeeper',
-    source: 'https://player.vimeo.com/video/109855756?color=ff0179&title=0&byline=0&portrait=0',
-    _id: '123',
-  },
-  {
-    title: '4.1 The Got-A-Minute Manager',
-    source: 'https://player.vimeo.com/video/109259708?title=0&byline=0&portrait=0',
-    _id: '456',
-  },
-  {
-    title: '5.3 I am a fake title',
-    source: 'https://player.vimeo.com/video/109259708?title=0&byline=0&portrait=0',
-    _id: '456',
-  },
-];
-
 const Div = styled.div`
   height: 506px;
   width: 100%;
@@ -30,7 +12,7 @@ const Div = styled.div`
 
 const VideoList = ({ videos }) => (
   <Div>
-    {mock.map(video => <VideoListItem key={video._id} video={video} />)}
+    {videos.map(video => <VideoListItem key={video._id} video={video} />)}
   </Div>
 );
 
